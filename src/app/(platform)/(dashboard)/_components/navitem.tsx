28329449df8ea587975abfaa33053549cd943809
Owain Williams <owain@andersdigital.com>
2023-11-18 11:38:45 +0000

    feat: workspace settings

--- src/app/(platform)/(dashboard)/_components/navitem.tsx
+++ src/app/(platform)/(dashboard)/_components/navitem.tsx
@@ -11,6 +11,7 @@ import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -103,5 +104,16 @@ export const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
   );
 };
